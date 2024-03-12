119fd0325777a7c8e106cbb15c92f03e56939407
Michael <michael@uplord.co.uk>
2024-03-12 20:25:49 +0000

    eslint

--- pages/index.vue
+++ pages/index.vue
@@ -11,10 +11,15 @@
       <h1>Login</h1>
       <form @submit.prevent="login">
         <div class="field">
-          <input type="email" v-model="email" placeholder="Email" required />
+          <input v-model="email" type="email" placeholder="Email" required />
         </div>
         <div class="field">
-          <input type="password" v-model="password" placeholder="Password" required />
+          <input
+            v-model="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
         </div>
         <button type="submit">Login</button>
         <button @click="state = 'register'">Register</button>
@@ -24,13 +29,23 @@
       <h1>Register</h1>
       <form @submit.prevent="register">
         <div class="field">
-          <input type="email" v-model="email" placeholder="Email" required />
+          <input v-model="email" type="email" placeholder="Email" required />
         </div>
         <div class="field">
-          <input type="password" v-model="password" placeholder="Password" required />
+          <input
+            v-model="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
         </div>
         <div class="field">
-          <input type="password" v-model="confirmPassword" placeholder="Confirm password" required />
+          <input
+            v-model="confirmPassword"
+            type="password"
+            placeholder="Confirm password"
+            required
+          />
         </div>
         <button type="submit">Register</button>
         <button @click="state = 'login'">Login</button>
@@ -40,11 +55,17 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { ref, onMounted } from "vue"
+import { initializeApp } from "firebase/app"
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "firebase/auth"
 
-const config = useRuntimeConfig();
+const config = useRuntimeConfig()
 
 const firebaseConfig = {
   apiKey: config.public.firebaseApiKey,
@@ -52,62 +73,68 @@ const firebaseConfig = {
   projectId: config.public.firebaseProjectId,
   storageBucket: config.public.firebaseStorageBucket,
   messagingSenderId: config.public.firebaseMessagingSenderId,
-  appId: config.public.firebaseApiId,
-};
+  appId: config.public.firebaseApiId
+}
 
-const app = initializeApp(firebaseConfig);
-const email = ref('');
-const password = ref('');
-const confirmPassword = ref('');
-const user = ref(null);
-const state = ref('login');
-const auth = getAuth();
+initializeApp(firebaseConfig)
+const email = ref("")
+const password = ref("")
+const confirmPassword = ref("")
+const user = ref(null)
+const state = ref("login")
+const auth = getAuth()
 
 onMounted(() => {
   // Check for stored login state on component mount
-  if (localStorage.getItem('loggedIn')) {
-    user.value = auth.currentUser;
+  if (localStorage.getItem("loggedIn")) {
+    user.value = auth.currentUser
   }
-});
+})
 
 const login = async () => {
   try {
-    const credentials = await signInWithEmailAndPassword(auth, email.value, password.value);
+    const credentials = await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    )
     // Redirect or do something after successful login
-    user.value = credentials.user;
-    localStorage.setItem('loggedIn', 'true');
+    user.value = credentials.user
+    localStorage.setItem("loggedIn", "true")
   } catch (error) {
-    console.error('Error logging in:', error);
-    // Handle error, e.g., show error message
+    console.error("Error logging in:", error)
   }
-};
+}
 
 const register = async () => {
   if (password.value !== confirmPassword.value) {
-    console.log('Passwords dont match')
+    console.log("Passwords dont match")
   } else {
     try {
-      const credentials = await createUserWithEmailAndPassword(auth, email.value, password.value);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      )
       // Redirect or do something after successful login
-      user.value = credentials.user;
-      localStorage.setItem('loggedIn', 'true');
+      user.value = credentials.user
+      localStorage.setItem("loggedIn", "true")
     } catch (error) {
-      console.error('Error logging in:', error);
+      console.error("Error logging in:", error)
       // Handle error, e.g., show error message
     }
   }
-};
+}
 
 const logout = async () => {
-  await signOut(auth);
-  user.value = null;
+  await signOut(auth)
+  user.value = null
   // Clear login state
-  localStorage.removeItem('loggedIn');
-};
+  localStorage.removeItem("loggedIn")
+}
 
 // Listen for authentication state changes
 onAuthStateChanged(auth, (currentUser) => {
-  user.value = currentUser;
-});
-
+  user.value = currentUser
+})
 </script>
